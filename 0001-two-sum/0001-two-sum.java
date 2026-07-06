1b0e96263575f496825c88400541939957b7883d
Shourya Sahu <sahushourya.2005@gmail.com>
2026-07-07 00:13:36 +0530

    Time: 45 ms (25.31%), Space: 47.3 MB (10.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
                 }
             }
         }
         // If no valid pair is found, return an empty array instead of null
-        return new int[] {};
+        return ans;
     }
 }
