9f5e0ad3f7c4ff8c144a3aaf5c5f29c3e15538c2
Shourya Sahu <sahushourya.2005@gmail.com>
2026-07-14 13:49:17 +0530

    Time: 0 ms (100%), Space: 44.6 MB (51.65%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Find the pivot
+        // The pivot is the first index from the right where nums[i] < nums[i + 1]
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If a pivot exists, find the smallest element
+        // greater than nums[i] by scanning from the right
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // Step 3: Reverse the suffix to get the smallest arrangement
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
