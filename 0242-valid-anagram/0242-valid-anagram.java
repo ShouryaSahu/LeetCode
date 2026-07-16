69de9f751d38b03e5cb15d395d5a3a23b797bdb9
Shourya Sahu <sahushourya.2005@gmail.com>
2026-07-16 11:43:27 +0530

    Time: 4 ms (82.21%), Space: 46.4 MB (42.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,11 +7,11 @@ class Solution {
        }
        Arrays.sort(arr1);
        Arrays.sort(arr2);
-       for(int i = 0 ; i < arr1.length ; i++){
-        if(arr1[i] != arr2[i]){
-            return false;
-        }
-       }
-       return true;
+    //    for(int i = 0 ; i < arr1.length ; i++){
+    //     if(arr1[i] != arr2[i]){
+    //         return false;
+    //     }
+    //    }
+       return Arrays.equals(arr1, arr2);    
     }
 }
