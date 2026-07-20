4e9059f888268210b221c7fa7c410d2e81a5459f
Shourya Sahu <sahushourya.2005@gmail.com>
2026-07-20 12:14:15 +0530

    Time: 495 ms (3.56%), Space: 63.1 MB (16.77%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,14 +3,29 @@ class Solution {
                int n = nums.length;
         k = k % n;
 
-        int[] temp = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        int total = list.size();
+        for(int i = 0 ; i < k ; i++){
+            int lastElement = list.get(total - 1);
+            list.remove(total - 1);
+            list.add(0 , lastElement);
         }
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = temp[i];
+        for(int i = 0 ; i < total ; i++){
+            nums[i] = list.get(i);
         }
+
+        // int[] temp = new int[n];
+
+        // for (int i = 0; i < n; i++) {
+        //     temp[(i + k) % n] = nums[i];
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        //     nums[i] = temp[i];
+        // }
     }
 }
