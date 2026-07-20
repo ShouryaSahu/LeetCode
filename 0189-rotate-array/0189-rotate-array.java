fa59e6ddf20ab17ec4f5d66f1225cb6a8f62423f
Shourya Sahu <sahushourya.2005@gmail.com>
2026-07-20 12:15:06 +0530

    Time: 1 ms (41.19%), Space: 63 MB (20.39%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,29 +3,32 @@ class Solution {
                int n = nums.length;
         k = k % n;
 
-        List<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            list.add(num);
-        }
-        int total = list.size();
-        for(int i = 0 ; i < k ; i++){
-            int lastElement = list.get(total - 1);
-            list.remove(total - 1);
-            list.add(0 , lastElement);
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
         }
 
-        for(int i = 0 ; i < total ; i++){
-            nums[i] = list.get(i);
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
         }
 
-        // int[] temp = new int[n];
 
-        // for (int i = 0; i < n; i++) {
-        //     temp[(i + k) % n] = nums[i];
+        // ANOTHER METHOD...    
+        // List<Integer> list = new ArrayList<>();
+        // for (int num : nums) {
+        //     list.add(num);
+        // }
+        // int total = list.size();
+        // for(int i = 0 ; i < k ; i++){
+        //     int lastElement = list.get(total - 1);
+        //     list.remove(total - 1);
+        //     list.add(0 , lastElement);
         // }
 
-        // for (int i = 0; i < n; i++) {
-        //     nums[i] = temp[i];
+        // for(int i = 0 ; i < total ; i++){
+        //     nums[i] = list.get(i);
         // }
+
     }
 }
