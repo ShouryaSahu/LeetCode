c9109bbc36d641b33c6b139209773dbc6cfe4a35
Shourya Sahu <sahushourya.2005@gmail.com>
2026-07-25 11:57:16 +0530

    Time: 1 ms (100%), Space: 42.4 MB (92.77%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,17 +1,37 @@
 class Solution {
     public int maxProduct(int n) {
-        List<Integer> list = new ArrayList<>();
+
         int max = 0;
+        int secondMax = 0;
         while(n > 0){
             int digit = n % 10;
-            list.add(0, digit);
+            if(digit > max){
+                secondMax = max;
+                max = digit;
+            }else if(digit > secondMax){
+                secondMax = digit;
+            }
             n /= 10;
         }
+        return max * secondMax;
 
-        Integer[] arr = list.toArray(new Integer[list.size()]);
-        Arrays.sort(arr);
-        int l = arr.length;
-        return (arr[l - 1] * arr[l-2]);
+
+
+
+        // List<Integer> list = new ArrayList<>();
+        // int max = 0;
+        // while(n > 0){
+        //     int digit = n % 10;
+        //     list.add(0, digit);
+        //     n /= 10;
+        // }
+        // CONVERTING LIST INTO ARRAY 
+        // Integer[] arr = list.toArray(new Integer[list.size()]);
+        // Arrays.sort(arr);
+        // int l = arr.length;
+        // return (arr[l - 1] * arr[l-2]);
+
+        // ANOTHER METHOD
         // for(int i=0; i < list.size() ; i++){
         //     for(int j = i+1 ; j < list.size() ; j++){
         //         max = Math.max(max, (list.get(i) * list.get(j)));
