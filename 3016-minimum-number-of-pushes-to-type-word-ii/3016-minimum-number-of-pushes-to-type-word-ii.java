cb1d83a5beedfd5db066e9da288c1b048878c41f
Shourya Sahu <sahushourya.2005@gmail.com>
2026-07-31 11:00:31 +0530

    Time: 85 ms (5.49%), Space: 48 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -5,16 +5,22 @@ class Solution {
             if(map.containsKey(ch)) map.put(ch, map.get(ch) + 1);
             else map.put(ch, 1);
         }
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>(map.values());
         
-        for(int value : map.values()){
-            list.add(value);
-        }
+        // for(int value : map.values()){
+        //     list.add(value);
+        // }
+
         list.sort(Collections.reverseOrder());
 
+        // First 8 letters  -> 1 push
+        // Next 8 letters   -> 2 pushes
+        // Next 8 letters   -> 3 pushes
+        // Remaining letters -> 4 pushes
         int ans = 0;
         for(int i = 0 ; i < list.size() ; i++){
-            ans += list.get(i) * (i/8 + 1); 
+            int pushes = (i/8 + 1);
+            ans += list.get(i) * pushes; 
             // i/8 + 1 for count if i < 8 = 1 and if i > 8 = 2 ; i > 16 = 3
         }
 
