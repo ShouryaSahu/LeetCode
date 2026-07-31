5d7a0d90bf7773239f51488c4cac65059bc7cf74
Shourya Sahu <sahushourya.2005@gmail.com>
2026-08-01 01:36:33 +0530

    Time: 5 ms (72.48%), Space: 160 MB (55.28%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,34 +1,56 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int left = 0;
-        int right = nums.length - 1;
+        
+        int[] ans = new int[nums.length];
+        int smaller = 0;
+        int equal = 0;
+        for(int num : nums){
+            if(num < pivot) smaller++;
+            else if(num == pivot) equal++;
+        }
 
-        ArrayList<Integer> smaller = new ArrayList<>();
-        ArrayList<Integer> larger = new ArrayList<>();
-        ArrayList<Integer> same = new ArrayList<>();
-        ArrayList<Integer> combined = new ArrayList<>();
+        int smallerIndex = 0;
+        int equalIndex = smaller;
+        int largerIndex = smaller + equal;
 
         for(int num : nums){
-             if(num < pivot){
-                smaller.add(num);
-            }else if(num > pivot){
-                larger.add(num);
-            }else{
-                same.add(num);
-            }
+            if(num < pivot) ans[smallerIndex++] = num;
+            else if(num == pivot) ans[equalIndex++] = num;
+            else ans[largerIndex++] = num; 
         }
 
-        combined.addAll(smaller);
-        combined.addAll(same);
-        combined.addAll(larger);
+        return ans;
 
-        int[] arr = new int[combined.size()];
 
-        for (int i = 0; i < combined.size(); i++) {
-               arr[i] = combined.get(i);
-        }
 
-        return arr;
+        // NOT APPROPRIATE BUT RIGHT
+
+        // ArrayList<Integer> smaller = new ArrayList<>();
+        // ArrayList<Integer> larger = new ArrayList<>();
+        // ArrayList<Integer> same = new ArrayList<>();
+        // ArrayList<Integer> combined = new ArrayList<>();
+
+        // for(int num : nums){
+        //      if(num < pivot){
+        //         smaller.add(num);
+        //     }else if(num > pivot){
+        //         larger.add(num);
+        //     }else{
+        //         same.add(num);
+        //     }
+        // }
+
+        // combined.addAll(smaller);
+        // combined.addAll(same);
+        // combined.addAll(larger);
+
+        // int[] arr = new int[combined.size()];
+
+        // for (int i = 0; i < combined.size(); i++) {
+        //        arr[i] = combined.get(i);
+        // }
+
+        // return arr;
 
     }
 }
