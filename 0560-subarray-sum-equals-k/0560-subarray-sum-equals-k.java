de15ce3325702c0314bb5cc880940c85a476826b
Shourya Sahu <sahushourya.2005@gmail.com>
2026-08-02 02:06:34 +0530

    Time: 26 ms (34.59%), Space: 48.8 MB (59.59%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,19 +1,38 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        if(nums.length == 1 && nums[0] == k){
-            return 1;
-        }
+        // if(nums.length == 1 && nums[0] == k){
+        //     return 1;
+        // }
+        // int count = 0;
+        // int sum = 0;
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     sum += nums[i];
+        //     if(sum == k) count++;
+        //     for(int j = i+1 ; j < nums.length; j++){
+        //         sum += nums[j];
+        //         if(sum == k) count++;
+        //     }
+        //     sum = 0;
+        // }
+        // return count;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1); //Initialized starting 
+
         int count = 0;
-        int sum = 0;
-        for(int i = 0 ; i < nums.length ; i++){
-            sum += nums[i];
-            if(sum == k) count++;
-            for(int j = i+1 ; j < nums.length; j++){
-                sum += nums[j];
-                if(sum == k) count++;
+        int prefixSum = 0;
+
+        for(int num : nums){
+            prefixSum += num;
+
+            if(map.containsKey(prefixSum - k)){
+                count += map.get(prefixSum - k);
             }
-            sum = 0;
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+            
         }
+
         return count;
     }
 }
