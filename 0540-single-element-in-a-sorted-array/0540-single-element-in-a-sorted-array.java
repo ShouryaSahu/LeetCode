b0c5709026b5c28a7c1c7b091c8def98b0ae0892
Shourya Sahu <sahushourya.2005@gmail.com>
2026-08-31 01:56:55 +0530

    Time: 0 ms (100%), Space: 52.9 MB (58.71%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,12 +1,31 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         // Using Bit Manipulation
-        int ans = 0;
-        for(int num: nums){
-            ans ^= num;
+        // int ans = 0;
+        // for(int num: nums){
+        //     ans ^= num;
+        // }
+        // return ans;
+
+        // Using Binary Search
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low < high){
+            int mid = low + (high - low) / 2;
+
+            //hamesha mid ko even index pr rakhenge kyuki jb tk single element nhi aa jata tb tk hrr duplicate element ka first element even index pr hoga aur second element odd index pr
+            if(mid % 2 == 1){
+                mid--;
+            }
+            if(nums[mid] == nums[mid+1]){ // agr yeah sahi hai mtlb mid se phle ke saare element duplicate present hai array mei toh uske baad check krenge
+                low = mid + 2;
+            }else{ // agr pattern tuta iska mtlb single element mid se phle hai...
+                high = mid;
+            }
         }
 
-        return ans;
+        return nums[low];
 
 
 
