c4b8c7c1102f18c0b55f2f00c263627ba1828996
Shourya Sahu <sahushourya.2005@gmail.com>
2026-09-10 00:33:29 +0530

    Time: 11 ms (40.75%), Space: 47 MB (34.58%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : magazine.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(char ch : ransomNote.toCharArray()){
+            if(!map.containsKey(ch)){ // Agar character hi nhi hai toh ransomNote ka magazine mei
+                return false;
+            }else{
+                map.put(ch, map.get(ch) - 1);
+                if(map.get(ch) < 0) return false;
+            }
+        }
+
+        return true;
+    }
+}
