63e9629726403019ca72d0602b287121bace9548
Shourya Sahu <sahushourya.2005@gmail.com>
2026-09-20 17:53:22 +0530

    Time: 1 ms (100%), Space: 43.9 MB (67.44%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,19 +1,32 @@
 class Solution {
     public int reverseDegree(String s) {
-        String alphabets = "abcdefghijklmnopqrstuvwxyz";
-        HashMap<Character, Integer> map = new HashMap<>();
-        int position = 26;
-        for(char ch : alphabets.toCharArray()){
-            map.put(ch, position--);
-        }
-
-        int index = 1;
         int degree = 0;
-        for(char ch: s.toCharArray()){
-            int product = map.get(ch) * index++;
+        for(int i = 0; i < s.length() ; i++){
+            int index = s.charAt(i) - 'a';
+            int reverseIndex = 26 - index;
+            int product = (i + 1) * reverseIndex;
             degree += product;
         }
 
         return degree;
     }
 }
+
+
+    //     String alphabets = "abcdefghijklmnopqrstuvwxyz";
+    //     HashMap<Character, Integer> map = new HashMap<>();
+    //     int position = 26;
+    //     for(char ch : alphabets.toCharArray()){
+    //         map.put(ch, position--);
+    //     }
+
+    //     int index = 1;
+    //     int degree = 0;
+    //     for(char ch: s.toCharArray()){
+    //         int product = map.get(ch) * index++;
+    //         degree += product;
+    //     }
+
+    //     return degree;
+    // }
+// }
